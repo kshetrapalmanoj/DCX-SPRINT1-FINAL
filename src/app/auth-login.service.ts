2ea1146e50f26fedb340f1f8ca1d812dad0ac8af
Manoj Kshetrapal <kshetrapalmanoj@gmail.com>
2021-04-21 14:31:30 +0530

    Update auth-login.service.ts

--- src/app/auth-login.service.ts
+++ src/app/auth-login.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { from, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthLoginService {
   constructor(private http: HttpClient) {}
-  url = 'https://dcx-app-server.herokuapp.com/api/developer/login';
+  url = `${environment.baseUrl}/api/developer/login`;
   head = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
   loginDeveloper(data): Observable<any> {
     return this.http.post<any>(this.url, data, this.head);
