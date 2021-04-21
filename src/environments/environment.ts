07ce79b93dbd93eabb332a37a2bfa86f7e49a73f
Manoj Kshetrapal <kshetrapalmanoj@gmail.com>
2021-04-21 08:19:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  url: 'https://dcx-app-server.herokuapp.com',
+  production: false,
 };
 
 /*
